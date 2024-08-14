a311e665d643bd76a53e72c5fb5a503118df46de
daleboy <785909868@qq.com>
2024-08-14 18:46:35 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import CourseGoal from "./components/CourseGoal";
+
+function App() {
+  return (
+    <main>
+      <CourseGoal />
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/CourseGoal.tsx
@@ -0,0 +1,13 @@
+function CourseGoal(){
+    return(
+        <article>
+            <div>
+                <h1>TITLE</h1>
+                <p>DESCRIPTION</p>
+            </div>
+            <button>Delete</button>
+        </article>
+    )
+}
+
+export default CourseGoal;
